16dac3247d40c85adf978c4bb0a59def5bd3f70a
Chris Partridge <chris@partridge.tech>
2022-12-26 12:57:35 -0500

    Updates

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This project is originally by [da_667](https://github.com/da667), with contribut
 
 ### Example
 
-This script supports TLDs, and FQDNs up to four subdomains deep - see the example below, in addition to the sampledns.txt and sample.rules files provided with this script.
+This script supports TLDs and FQDNs - see the example below, or try the sampledns.txt and sample.rules files provided with this repository.
 
 **Example input:**
 
@@ -25,11 +25,11 @@ stupidlylongsubdomain.lol.wutski.biz
 **...becomes:**
 
 ```
-alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain .pw"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|02|pw|00|"; fast_pattern:only; metadata:service dns;  sid:1000000; rev:1;)
-alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain evilcorp.co"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|08|evilcorp|02|co|00|"; fast_pattern:only; metadata:service dns;  sid:1000001; rev:1;)
-alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain www.evil.com"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|03|www|04|evil|03|com|00|"; fast_pattern:only; metadata:service dns;  sid:1000002; rev:1;)
-alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain seemstoteslegit.notreally.tk"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|0F|seemstoteslegit|09|notreally|02|tk|00|"; fast_pattern:only; metadata:service dns;  sid:1000003; rev:1;)
-alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain stupidlylongsubdomain.lol.wutski.biz"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|15|stupidlylongsubdomain|03|lol|06|wutski|03|biz|00|"; fast_pattern:only; metadata:service dns;  sid:1000004; rev:1;)
+alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain .pw"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|02|pw|00|"; fast_pattern:only; metadata:service dns; sid:1000000; rev:1;)
+alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain evilcorp.co"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|08|evilcorp|02|co|00|"; fast_pattern:only; metadata:service dns; sid:1000001; rev:1;)
+alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain www.evil.com"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|03|www|04|evil|03|com|00|"; fast_pattern:only; metadata:service dns; sid:1000002; rev:1;)
+alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain seemstoteslegit.notreally.tk"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|0F|seemstoteslegit|09|notreally|02|tk|00|"; fast_pattern:only; metadata:service dns; sid:1000003; rev:1;)
+alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain stupidlylongsubdomain.lol.wutski.biz"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|15|stupidlylongsubdomain|03|lol|06|wutski|03|biz|00|"; fast_pattern:only; metadata:service dns; sid:1000004; rev:1;)
 ```
 
 These are all properly formatted Snort rules ready to be pushed to a sensor.
@@ -37,5 +37,6 @@ These are all properly formatted Snort rules ready to be pushed to a sensor.
 ### Notes
 
 * Please note that none of these sample domains are malicious. They are samples for testing only.
-* The domain list input file should not have ANY trailing spaces on any of the individual user-agent lines. Additionally, there should be ZERO blank lines in the domain list file that will be used to generate the snort rules. If the script encounters any FQDNs greater than 4 subdomains deep (such as "lol.wut.you.doin.it [5-level subdomain]") the script will skip over them, notify the user, and print it to the terminal. 
-* If you think this is a concern, the Mandiant APT1 report has over 2000 domains as IOCs associated with the campaign. dns2snort only encountered two FQDNs with 5+ subdomains; dns2snort successfully created rules for every other domain in the list of IOCs. Maybe you can look over my code and implement a way to support FQDNs of varying length? I'm not good enough at python to do this currently without a huge CF of if/then statements. Help is welcome.
+* Some additional changes have been made from da_667's version of dns2snort, these are:
+  * The subdomain limitation has been removed.
+  * There is no longer an argument to allow removing `www.` from domains.
--- dns2snort.py
+++ dns2snort.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 import argparse
-import textwrap
-import re
 
 description = """Given a file containing a list of FQDNs, quickly generate Snort rules for those domains.\n
 Brought to you by @da_667, @botnet_hunter, @3XPlo1T2, and tweedge."""
@@ -12,7 +10,7 @@ parser.add_argument(
     "-i",
     dest="infile",
     required=True,
-    help="The name of the file containing a list of domains, One domain per line.",
+    help="The name of the file containing a list of domains, one domain per line.",
 )
 parser.add_argument(
     "-o",
@@ -26,13 +24,6 @@ parser.add_argument(
     type=int,
     required=True,
     help="The Snort SID to start numbering incrementally at. This number should be between 1000000 and 2000000.",
-)
-parser.add_argument(
-    "-w",
-    dest="www",
-    required=False,
-    action="store_true",
-    help="Remove the 'www' subdomain from domains that have it.",
 )
 args = parser.parse_args()
 
@@ -50,116 +41,42 @@ if why_failed:
 # rules_out_file is the file we will be outputting our rules to.
 # domains_in_file is the file we will read a list of domains from.
 # This script iterates through each line (via for line loop) and splits on periods (.), creating a list for each line.
-# The script calculates the segments of the domain in question (can handle 1-4 segments -- e.g. .ru (1 segments, TLD) all the way to this.is.evil.ru (4 segments))
-# Each segment of a domain has it's string length calculated and converted to hex.
+# The script splits each domain into its component parts (TLD, domain, subdomain ...)
+# Each segment of a domain has its string length calculated and converted to hex.
 # If the segment is less than or equal to 0xf, this is converted to "0f" (padded with a zero, since snort rules expect this)
 # The hexidecmal letter is converted to upper case, and the rule is written to a file.
 # after the rule is written the SID number is incremented by 1 for the next rule.
 
-with open(args.outfile, "w") as rules_out_file:
-    with open(args.infile, "r") as domains_in_file:
-        for line in domains_in_file:
-            domain = line.rstrip()
-            if args.www == True:
-                domain = re.sub("^www\.", "", domain, flags=re.IGNORECASE)
-            segment = domain.split(".")
-            # try/except fixes a bug with TLD rule creation where segment has 2 elements and element 0 is '' for some reason.
-            try:
-                segment.remove("")
-            except ValueError:
-                pass
-            if len(segment) == 1:
-                sega = (hex(len(segment[0])))[2:]
-                if int(len(sega)) == 1:
-                    sega = "0%s" % sega
-                rule = (
-                    'alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain %s"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|%s|%s|00|"; fast_pattern:only; metadata:service dns;  sid:%s; rev:1;)\n'
-                    % (domain, sega.upper(), segment[0], args.sid)
-                )
-                rules_out_file.write(rule)
-                print(rule)
-                args.sid += 1
-            elif len(segment) == 2:
-                sega = (hex(len(segment[0])))[2:]
-                if int(len(sega)) == 1:
-                    sega = "0%s" % sega
-                segb = (hex(len(segment[1])))[2:]
-                if int(len(segb)) == 1:
-                    segb = "0%s" % segb
-                rule = (
-                    'alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain %s"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|%s|%s|%s|%s|00|"; fast_pattern:only; metadata:service dns;  sid:%s; rev:1;)\n'
-                    % (
-                        domain,
-                        sega.upper(),
-                        segment[0],
-                        segb.upper(),
-                        segment[1],
-                        args.sid,
-                    )
-                )
-                rules_out_file.write(rule)
-                print(rule)
-                args.sid += 1
-            elif len(segment) == 3:
-                sega = (hex(len(segment[0])))[2:]
-                if int(len(sega)) == 1:
-                    sega = "0%s" % sega
-                segb = (hex(len(segment[1])))[2:]
-                if int(len(segb)) == 1:
-                    segb = "0%s" % segb
-                segc = (hex(len(segment[2])))[2:]
-                if int(len(segc)) == 1:
-                    segc = "0%s" % segc
-                rule = (
-                    'alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain %s"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|%s|%s|%s|%s|%s|%s|00|"; fast_pattern:only; metadata:service dns;  sid:%s; rev:1;)\n'
-                    % (
-                        domain,
-                        sega.upper(),
-                        segment[0],
-                        segb.upper(),
-                        segment[1],
-                        segc.upper(),
-                        segment[2],
-                        args.sid,
-                    )
-                )
-                rules_out_file.write(rule)
-                print(rule)
-                args.sid += 1
-            elif len(segment) == 4:
-                sega = (hex(len(segment[0])))[2:]
-                if int(len(sega)) == 1:
-                    sega = "0%s" % sega
-                segb = (hex(len(segment[1])))[2:]
-                if int(len(segb)) == 1:
-                    segb = "0%s" % segb
-                segc = (hex(len(segment[2])))[2:]
-                if int(len(segc)) == 1:
-                    segc = "0%s" % segc
-                segd = (hex(len(segment[3])))[2:]
-                if int(len(segd)) == 1:
-                    segd = "0%s" % segd
-                rule = (
-                    'alert udp $HOME_NET any -> $EXTERNAL_NET 53 (msg:"BLACKLIST DNS domain %s"; flow:to_server; byte_test:1,!&,0xF8,2; content:"|%s|%s|%s|%s|%s|%s|%s|%s|00|"; fast_pattern:only; metadata:service dns;  sid:%s; rev:1;)\n'
-                    % (
-                        domain,
-                        sega.upper(),
-                        segment[0],
-                        segb.upper(),
-                        segment[1],
-                        segc.upper(),
-                        segment[2],
-                        segd.upper(),
-                        segment[3],
-                        args.sid,
-                    )
-                )
-                print(rule)
-                rules_out_file.write(rule)
-                args.sid += 1
-            else:
-                print(
-                    "the number of segments in the domain %s is greater than 4. Skipping."
-                    % domain
-                )
-                pass
+rules_out_file = open(args.outfile, "w")
+domains_in_file = open(args.infile, "r")
+sid = args.sid
+
+for line in domains_in_file:
+    domain = line.rstrip()
+
+    # skip empty lines
+    if domain == "":
+        continue
+
+    segments = domain.split(".")
+
+    # remove blank strings in index 0 (ex. where ".tld" is split into ["", "tld"])
+    if segments[0] == "":
+        segments = segments[1:]
+
+    content = ""
+    for segment in segments:
+        segment_len_hex = hex(len(segment))[2:]
+        if len(segment_len_hex) == 1:
+            segment_len_hex = "0%s" % segment_len_hex
+        content += f"|{segment_len_hex.upper()}|{segment}"
+
+    filter = "alert udp $HOME_NET any -> $EXTERNAL_NET 53"
+    message = f'msg:"BLACKLIST DNS domain {domain}";'
+    detect = f'flow:to_server; byte_test:1,!&,0xF8,2; content:"{content}|00|"; fast_pattern:only;'
+    metadata = f"metadata:service dns; sid:{sid}; rev:1;"
+
+    rule = f"{filter} ({message} {detect} {metadata})\n"
+    rules_out_file.write(rule)
+
+    sid += 1
