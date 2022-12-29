c8dff8e0a1d5671317ec13dec2b330408a94e52e
Chris Partridge <chris@partridge.tech>
2022-12-28 21:57:07 -0500

    Enable custom metadata

--- dns2snort.py
+++ dns2snort.py
@@ -26,6 +26,18 @@ parser.add_argument(
     required=True,
     help="The Snort SID to start numbering incrementally at. This number should be between 1000000 and 2000000.",
 )
+parser.add_argument(
+    "-m",
+    dest="message",
+    type=str,
+    help="Optional: A custom message to include in the rule",
+)
+parser.add_argument(
+    "-u",
+    dest="reference",
+    type=str,
+    help="Optional: A reference URL to include in the rule",
+)
 args = parser.parse_args()
 
 # This is a small check to ensure -s is set to a valid value between one and two million - the local rules range.
@@ -70,14 +82,28 @@ for line in domains_in_file:
         segment_len_hex = hex(len(segment))[2:]
         if len(segment_len_hex) == 1:
             segment_len_hex = "0%s" % segment_len_hex
-        content += f"|{segment_len_hex.upper()}|{segment}"
+        content += f"|{segment_len_hex}|{segment}"
     content = f'"{content}|00|"'
 
-    filter = "alert udp $HOME_NET any -> any 53"
+    # inject any custom metadata
     message = f'msg:"dns2snort banned DNS domain {domain}";'
+    if args.message:
+        arg_message = args.message
+        if "{domain}" in arg_message:
+            imploded_domain = " .".join(segments)
+            arg_message = arg_message.replace("{domain}", imploded_domain)
+        message = f'msg:"{arg_message}";'
+
+    ref = ""
+    if args.reference:
+        arg_ref = args.reference.replace("https://", "")
+        ref = f"reference:url,{arg_ref}; "
+
+    # construct the rule
+    filter = "alert udp $HOME_NET any -> any 53"
     dns_queries_only = 'content:"|01|"; offset:2; depth:1; content:"|00 01 00 00 00 00 00|"; distance:1; within:7;'
     detect = f'content:{content}; nocase; distance:0; fast_pattern;'
-    metadata = f"metadata:service dns; sid:{sid}; rev:1;"
+    metadata = f"metadata:service dns; sid:{sid}; {ref}rev:1;"
 
     rule_string = f"{filter} ({message} {dns_queries_only} {detect} {metadata})\n"
     parsed_rule = rule.parse(rule_string)
